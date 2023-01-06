0af7c4da61009e2b9a1863295d44a65f8a5c10aa
Nisal Vimukthi <50359663+Neeka0909@users.noreply.github.com>
2023-01-06 10:27:03 +0530

    Readers data entry form created

--- app/src/components/readerform.component.js
+++ app/src/components/readerform.component.js
@@ -1,6 +1,39 @@
+// React-Bootstrap components are imported from the react-bootstrap package.
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/esm/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+
 function ReaderForm() {
     return (
-        <h4>Reader form</h4>
+        <Row className='m-5 p-3 justify-content-md-center'>
+            <Col md='4'>
+                <Form className="border p-3">
+                    <h3>Meater Reading Data</h3>
+                    <Form.Group controlId="formBasicEmail" className='mb-2'>
+                        <Form.Label>Customer Account Number</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Customer Account Number" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword" className='mb-3'>
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control type="date" min={Date()} placeholder="Date" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail" className='mb-2'>
+                        <Form.Label>Meater Reading</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Customer Meter Reading" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Save
+                    </Button>
+                </Form>
+
+            </Col>
+        </Row>
+
     );
 }
 
