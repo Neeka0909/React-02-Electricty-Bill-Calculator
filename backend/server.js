3d5809abb57c14e0dbd95fc0436e63651273d538
Nisal Vimukthi <50359663+Neeka0909@users.noreply.github.com>
2023-01-06 12:02:14 +0530

    Create backend and established connection with mongodb-atlas

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 // Modules
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 //configs
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3500
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+
+
+
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// establish connection to database
+mongoose.connect(uri);
+mongoose.set('strictQuery', false)
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+
 
 
